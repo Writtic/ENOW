4f6279595d7af1fe5b1f338d2e724194edcca85d
writtic <writtic@gmail.com>
2016-07-10 19:42:57 +0900

    테스트용 두번째 커밋

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+# 로그를 기록하기 위한 메소드
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    # 이벤트 로그를 기록
+    logger.info('got event{}'.format(event))
+    # event는 딕셔너리형태로 넘어옴. 커멘드(/wiki) 뒤의 텍스트를 받아오려면 event['text']
+    text = event['text']
+    # 리턴은 JSON 타입으로 맞춰준다.
+    return {
+        "response_type": "in_channel",
+        "text": "I found some about \"" + text + "\"",
+        "attachments": [
+            {
+                "text": "For more info please visit to \nhttps://ko.wikipedia.org/wiki/" + text,
+                "color": "#7CD197"
+            }
+        ]
+    }
+"""
+Lambda > Functions > 내 함수 > Configuration > Handler 에서 input을
+[파일명.메소드명]으로 한다.
+예시 : lambda_function.lambda_handler
+"""
