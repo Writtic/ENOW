e309bc706491a6f2d8633cce66defbf190d49c1b
writtic <writtic@gmail.com>
2016-07-12 17:15:15 +0900

    Delete Anonymous function

--- Jun/nodejs/main.js
+++ Jun/nodejs/main.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type' : 'text/plain' });
-  res.end('Hello World');
-});
+var server = http.createServer(helloResponse);
+
+function helloResponse(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}
 
 server.listen(8000);
