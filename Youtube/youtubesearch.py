261230c3455df0304b43a6758940e873ecd0504c
Kihwan Kim <liamgall@naver.com>
2016-07-19 17:41:45 +0900

    Update youtubesearch.py

--- Youtube/youtubesearch.py
+++ Youtube/youtubesearch.py
@@ -13,11 +13,8 @@ import shlex
 import argparse
 
 
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyCNvlc8dQVAEoKzINru5YkCYL_0NHgDmOo"
+# developer_key from google api
+DEVELOPER_KEY = "aaaaaa"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
